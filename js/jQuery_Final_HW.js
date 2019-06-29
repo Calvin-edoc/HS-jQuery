fd2dec9c6dc8582f8a631a1802ed05efd06f51f2
Calvin-edoc <calvin.edoc@gmail.com>
2019-06-29 22:07:45 +0800

    jQuery_Final_HW

---
+++ js/jQuery_Final_HW.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    // 下拉式選單 begin
+    $('.dropdown').click(function (e) { 
+        e.stopPropagation();
+        
+        $(this).parent().find('.dropdown_open').slideToggle();        
+        $(this).toggleClass('active')
+        $(this).parent().siblings().find('.dropdown').removeClass('active');
+        $(this).parent().siblings().find('.dropdown_open').hide();      
+    });
+
+    $('html').click(function(){
+        $('.dropdown_open').hide();
+    });
+    // 下拉式選單 end
+
+    // Swiper begin
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        effect:'cube',
+        cubeEffect: {                       
+            shadow: false,
+        },
+        autoplay: {
+            delay: 2000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+
+        })
+    // Swiper end
+    
+    $('.back').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({scrollTop:0},700);
+    });
+        
+});
